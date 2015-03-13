688f7a2d4036596cf7792d3f44a08e9f276974d2
Liongold <beimaginativeegroup@gmail.com>
2015-03-13 20:19:08 +0100

    Fixing #14: Rewriting Options dialog and implement Preferences
    Rewritten the HTML of the Options dialog
    
    Add Preferences.js file
    
    Added module to contain all code related to preferences handling and included it in the Options dialog.
    
    Add and change Strings needed for new export dialog

---
+++ src/Preferences.js
@@ -0,0 +1,51 @@
+define(function (require, exports) {
+    "use strict";
+    
+    //Dependencies
+    var PreferencesManager = brackets.getModule("preferences/PreferencesManager");
+    var extensionPrefs = PreferencesManager.getExtensionPrefs("brackets-pdfexport");
+    var _ = brackets.getModule("thirdparty/lodash");
+    
+    /**
+     * @const
+     * @private
+     * The default preferences to be set on first extension run
+     * @object
+     */
+    var defaultPreferences = {
+        "topMargin": { type: "number", value: 72}, //
+        "leftMargin": { type: "number", value: 72 }, //
+        "rightMargin": { type: "number", value: 72 }, //
+        "bottomMargin": { type: "number", value: 72 }, //
+        "fontSize": { type: "number", value: 10 },
+        "rangeExport": { type: "string", value: "whole" },
+        "openAfterExport": { type: "boolean", value: 1 }
+    };
+    
+    /**
+     * If preferences are not, set the default ones right now
+     */
+    _.each(defaultPreferences, function (definition, key, type, value) {
+        extensionPrefs.definePreference(key, definition.type, definition.value);
+    });
+    
+    /**
+     * @param {!string} key
+     * @return {!string}
+     */
+    function get(key) {
+        return extensionPrefs.get(key);
+    }
+    
+    /**
+     * @param {!string} key
+     * @param {!string/boolean/number} value
+     */
+    function set(key, value) {
+        /* @TODO: Will be implemented later */
+    }
+    
+    //Define public API
+    exports.getPreference = get;
+    exports.setPreference = set;
+});
--- src/nls/root/strings.js
+++ src/nls/root/strings.js
@@ -6,6 +6,8 @@ define({
     "ERROR_UNSUPPORTED_FILE_MSG":   "The following file is not supported and can not be exported as a PDF document: {0}",
     "ERROR_UNSUPPORTED_FILE_TITLE": "Unsupported File",
     "LABEL_FONTSIZE":               "Font Size",
-    "LABEL_SELECTION":              "Selection",
-    "LABEL_CURRENT_FILE":           "Current File"
+    "LABEL_EXPORT_SELECTION":       "Export Selection",
+    "LABEL_EXPORT_CURRENT_FILE":    "Export Current File",
+    "LABEL_MARGINS":                "Margins (in printer points)",
+    "LABEL_OPENPDF":                "Open PDF After Export"
 });
