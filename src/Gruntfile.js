33cd18efa6821a9000573b4d8af7e5d58f0d6be5
liongold <beimaginativeegroup@gmail.com>
2015-05-08 15:16:27 +0000

    Last step

--- src/Gruntfile.js
+++ src/Gruntfile.js
@@ -11,30 +11,76 @@ module.exports = function(grunt) {
             },
             all: ["**.js", "thirdparty/**.js", "nls/**/**.js"]
         },
-        /*requirejs: {
+        copy: {
             all: {
-                options: {
-                    name: "main",
-                    out: "../distribution/main.js",
-                    useStrict: true,
-                    optimise: "uglify2",
-                    uglify2: {}
-                }
+                files: [
+                    {
+                        expand: true,
+                        src: ["htmlContent/**"],
+                        dest: "../brackets-pdfexport"
+                    },
+                    {
+                        expand: true,
+                        src: ["nls/**"],
+                        dest: "../brackets-pdfexport"
+                    },
+                    {
+                        expand: true,
+                        src: ["thirdparty/**"],
+                        dest: "../brackets-pdfexport"
+                    },
+                    {
+                        cwd: "../",
+                        expand: true,
+                        src:["README.md"],
+                        dest: "../brackets-pdfexport",
+                        filter: "isFile"
+                    },
+                    {
+                        expand: true,
+                        src: ["*.json"],
+                        dest: "../brackets-pdfexport",
+                        filter: "isFile"
+                    },
+                    {
+                        cwd: "../",
+                        expand: true,
+                        src: ["LICENSE"],
+                        dest: "../brackets-pdfexport",
+                        filter: "isFile"
+                    }
+                ]
             }
-        }*/
+        },
+        uglify: {
+           options: {
+               mangle: false
+           },
+           all: {
+               files: {
+                   "../brackets-pdfexport/Dialogs.min.js": "Dialogs.js",
+                   "../brackets-pdfexport/FileSystemDomain.min.js": "FileSystemDomain.js",
+                   "../brackets-pdfexport/main.min.js": "main.js",
+                   "../brackets-pdfexport/PDFDocument.min.js": "PDFDocument.js",
+                   "../brackets-pdfexport/Preferences.min.js": "Preferences.js"
+               }
+           }
+           
+        },
         compress: {
             options: {
-                archive: "<%= pkg.name %>-<%= pkg.version %>.zip"
+                archive: "../../<%= pkg.name %>-<%= pkg.version %>.zip"
             },
-            src: ["*.js", "*.json", "thirdparty/**", "nls/**", "htmlContent/**", "**.md", "LICENSE"]
+            src: ["../brackets-pdfexport/**"]
         }
     });
 
     // Load tasks
     grunt.loadNpmTasks("grunt-contrib-jshint");
-    //grunt.loadNpmTasks("grunt-contrib-requirejs");
     grunt.loadNpmTasks("grunt-contrib-compress");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-copy");
 
     // Register Tasks 
-    grunt.registerTask("default", ["jshint", /*"requirejs", */"compress"]);
+    grunt.registerTask("default", ["jshint", "copy", "uglify", "compress"]);
 };
